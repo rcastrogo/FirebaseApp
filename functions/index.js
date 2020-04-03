78446a78df3eaab0e965cdf47dae5fdf73c7ebe8
Rafael Castro Gomez <rafael.castro@atos.net>
2020-04-03 18:19:54 +0200

    Functions - JWT

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,62 @@
-const functions = require('firebase-functions');
-const express   = require('express');
-const cors      = require('cors')({origin: true});
-const fetch     = require('node-fetch');
 
-const app  = express();
+// ===============================================================
+// Firebase
+// ===============================================================
+require('firebase/firebase-auth');
+const firebaseApp = require('firebase/app');
+const admin       = require('firebase-admin');
+// ===============================================================
+// Firebase auth
+// ===============================================================
+firebaseApp.initializeApp({
+  apiKey: "AIzaSyAIownX-ySe-_52p4JniFHPkAQ0Fn_McrE",
+  authDomain: "belloto-c925d.firebaseapp.com",
+  databaseURL: "https://belloto-c925d.firebaseio.com",
+  projectId: "belloto-c925d",
+  storageBucket: "belloto-c925d.appspot.com",
+  messagingSenderId: "931612603004",
+  appId: "1:931612603004:web:85d23bf6f0287b42f78ff9"
+});
+// ===============================================================
+// Firebase admin
+// ===============================================================
+let serviceAccount = require("./serviceAccountKey.json");
+admin.initializeApp({
+  databaseURL: "https://belloto-c925d.firebaseio.com",
+  credential: admin.credential.cert(serviceAccount)
+});
+
+// ===============================================================
+// Express middelware
+// ===============================================================
+const fetch   = require('node-fetch');
+const express = require('express');
+const cors    = require('cors')({origin: true});
+const app     = express();
+
+
+const validateFirebaseIdToken = async (req, res, next) => {
+  
+  if (!req.headers.authorization ||
+      !req.headers.authorization.startsWith('Bearer ')) {
+    res.status(403).send('Unauthorized');
+    return;
+  }
+  try {
+    let idToken = req.headers.authorization.split('Bearer ')[1];
+    req.user = await admin.auth().verifyIdToken(idToken);
+    next();
+    return;
+  } catch (error) {
+    res.status(403).send('Unauthorized');
+    return;
+  }
+
+};
+
+app.use(validateFirebaseIdToken);
 app.use(cors);
 
-exports.app = functions.https.onRequest(app);
 app.get('/books', (req, res) => {
   
   const cat  = req.query.category || '';
@@ -28,6 +78,40 @@ app.get('/books', (req, res) => {
 
 });
 
+app.get('/auth/token', async (req, res) => { 
+  firebaseApp.auth()
+             .signInWithEmailAndPassword(
+               req.query.username || '', 
+               req.query.password || ''
+             )
+             .then((result) => {
+               res.json(result.user.toJSON());
+             })
+             .catch((err) => {              
+               res.send(err);
+             });
+});
+
+app.get('/auth/data', async (req, res) => { 
+  if(!req.headers.authorization) {
+    return res.status(403)
+              .send({ message: "authorization: Bearer"});
+  }
+  admin.auth()
+       .verifyIdToken(req.headers.authorization.split(' ')[1] || '')
+       .then(function(result) {
+         res.send(result)
+       }).catch(function(error) {
+         res.send(error);
+       });
+
+})
+
+// ===============================================================
+// Firebase Functions
+// ===============================================================
+const functions = require('firebase-functions');
+
 exports.helloWorld = functions.https.onCall((data, context) => {
   //  if (!context.auth) return {status: 'error', code: 401, message: 'Not signed in'}
   //  return new Promise((resolve, reject) => {
@@ -39,26 +123,16 @@ exports.helloWorld = functions.https.onCall((data, context) => {
 });
 
 exports.getDate = functions.https.onRequest((request, response) => {
-  enableCors(request, response); 
-  response.setHeader('Content-Type', 'application/json')
-  response.status(200)
-          .send(
-    result({ 
-      success : true, 
-      date    : Date.now()
-    })
-  );
-})
-
-function result(data) {
-  return JSON.stringify({ data : data });
-}
-
-function enableCors(request, response) {
   response.set('Access-Control-Allow-Headers', 'Origin, Content-Type, authorization');
   response.set('Access-Control-Allow-Origin', request.headers.origin);
   response.set('Access-Control-Allow-Credentials', 'true');
-}
+  response.setHeader('Content-Type', 'application/json')
+  response.status(200)
+          .send({ success : true, 
+                  date    : Date.now()
+                });
+})
 
+exports.app = functions.https.onRequest(app);
 
 
